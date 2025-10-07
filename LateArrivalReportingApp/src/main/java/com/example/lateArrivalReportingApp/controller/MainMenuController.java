566e882a249f4d6aa4fee72b30ffc7b9f76e1b98
TakahiroTerao <familiar-20250310@outlook.com>
2025-10-07 16:36:55 +0900

    名前変更2

--- LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/controller/MainMenuController.java
+++ LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/controller/MainMenuController.java
@@ -1,4 +1,4 @@
-package com.example.bank.controller;
+package com.example.lateArrivalReportingApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
