3c3ccf7495442d8bf7b006340e6c04e7ecbca28d
TakahiroTerao <familiar-20250310@outlook.com>
2025-10-07 16:29:55 +0900

    あ

---
+++ LateArrivalReportingApp/src/main/java/com/example/bank/controller/TransferConfirmationController.java
@@ -0,0 +1,14 @@
+package com.example.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TransferConfirmationController {
+
+    @GetMapping("/transferConfirmation")
+    public String display() {
+        return "transferConfirmation";
+    }
+    
+}
