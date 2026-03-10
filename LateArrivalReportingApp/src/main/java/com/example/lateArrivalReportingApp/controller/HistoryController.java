71bf6fd200a93560592ef58774ea64736d977048
ryu-fukai <ryu-fukai@ac-tec.co.jp>
2026-03-10 17:41:28 +0900

    suzuyama:照会画面初期登録

--- LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/controller/HistoryController.java
+++ LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/controller/HistoryController.java
@@ -3,9 +3,11 @@ package com.example.lateArrivalReportingApp.controller;
 import com.example.lateArrivalReportingApp.model.CodeMst;
 import com.example.lateArrivalReportingApp.model.EmployeeMst;
 import com.example.lateArrivalReportingApp.model.TeamMst;
+import com.example.lateArrivalReportingApp.model.UserRolesMst;
 import com.example.lateArrivalReportingApp.repository.CodeMstRepository;
 import com.example.lateArrivalReportingApp.repository.EmployeeMstRepository;
 import com.example.lateArrivalReportingApp.repository.TeamMstRepository;
+import com.example.lateArrivalReportingApp.repository.UserRolesMstRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,23 +21,49 @@ public class HistoryController {
     private final CodeMstRepository codeMstRepository;
     private final TeamMstRepository teamMstRepository;
     private final EmployeeMstRepository employeeMstRepository;
+    private final UserRolesMstRepository userRolesMstRepository;
 
     public HistoryController(CodeMstRepository codeMstRepository, TeamMstRepository teamMstRepository,
-            EmployeeMstRepository employeeMstRepository) {
+            EmployeeMstRepository employeeMstRepository, UserRolesMstRepository userRolesMstRepository) {
         this.codeMstRepository = codeMstRepository;
         this.teamMstRepository = teamMstRepository;
         this.employeeMstRepository = employeeMstRepository;
+        this.userRolesMstRepository = userRolesMstRepository;
+        
     }
 
     @GetMapping("/employee/search")
     public String showSearchPage(Model model) {
+
+        // 社員ID取得
+        String empId = (String) model.getAttribute("empId");
+        // チームID取得
+        String empTermId = (String) model.getAttribute("empTermId");
+
+        // ユーザーロール取得
+        List<UserRolesMst> userRoles = userRolesMstRepository.findByGroupId(empId);
+        //model.addAttribute("userRoles", userRoles);
+
+        // 社員IDに紐づくロールIDが4(MEMBER)だった場合
+        if (userRoles.stream().anyMatch(r -> r.getRolesId().equals("MEMBER"))) {
+            // チームIDに紐づくチームマスタ情報取得
+            List<TeamMst> teams = teamMstRepository.findByTeamId(empTermId);
+        model.addAttribute("teams", teams);
+
+
+            //model.addAttribute("isAdmin", true);
+        // } else {
+        //     model.addAttribute("isAdmin", false);
+        }
+
+        
+        
+        
         // ユニット
         List<CodeMst> units = codeMstRepository.findByGroupId("UNIT");
         model.addAttribute("units", units);
 
-        // チーム
-        List<TeamMst> teams = teamMstRepository.findAll();
-        model.addAttribute("teams", teams);
+
 
         // 社員名
         List<EmployeeMst> employees = employeeMstRepository.findAll();
