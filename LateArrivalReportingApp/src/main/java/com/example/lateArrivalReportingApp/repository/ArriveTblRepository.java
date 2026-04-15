2514ea1c4516ec3860318f51cde6237e8e9221de
suzuyamaterao <familiar-20250310@outlook.com>
2026-04-15 17:03:24 +0900

    Change primary key type in ArriveTblRepository

--- LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/repository/ArriveTblRepository.java
+++ LateArrivalReportingApp/src/main/java/com/example/lateArrivalReportingApp/repository/ArriveTblRepository.java
@@ -1,8 +1,11 @@
 package com.example.lateArrivalReportingApp.repository;
 
 import com.example.lateArrivalReportingApp.model.ArriveTbl;
+import com.example.lateArrivalReportingApp.model.ArriveTblId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArriveTblRepository extends JpaRepository<ArriveTbl, String> {
+public interface ArriveTblRepository extends JpaRepository<ArriveTbl, ArriveTblId> {
+
     boolean existsByEmpIdAndContactDate(String empId, String contactDate);
+
 }
